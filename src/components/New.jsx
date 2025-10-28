c0925046a9bbaf2a58db4d822e5fd4745bda294b
Ru6ab <rubab.mehmod@gmail.com>
2025-10-28 19:33:05 +0500

    Resolved final updates after merge

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 
 export default function New() {
   return (
-<<<<<<< HEAD
-    <div></div>
-=======
     <div><h1>hi i am updated fromolder version</h1></div>
->>>>>>> old-version-fix
   )
 }
