a33bf7c46a6a4a0f4978e923ddbbf4474e491f74
Ru6ab <rubab.mehmod@gmail.com>
2025-11-03 20:32:20 +0500

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className="  h-[70px] bg-neutral-900 flex items-center justify-center ">
-  <p className="text-white text-[16px] font-medium">Made by me</p>
+  <p className="text-neutral-400 ibm-font  text-[16px] font-md">Made by me</p>
   </footer>
   )
 }
